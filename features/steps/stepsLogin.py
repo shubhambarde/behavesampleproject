efd19f7066c6d24049ff8709447d81d035d6cd1b
shubhambarde <shubhambarde837@gmail.com>
2025-08-20 12:57:39 +0530

    Refactor before_all function to remove redundant baseURL assignment and streamline environment handling

--- features/steps/stepsLogin.py
+++ features/steps/stepsLogin.py
@@ -2,15 +2,7 @@ from behave import given, when, then
 
 def before_all(context):
     print("Before all tests")
-    inputENV = context.config.userdata.get("ENV")
-    print("Environment: {inputENV}")
-    if inputENV == "sys":
-        context.baseURL = "https://www.sys.com"
-    elif inputENV == "dev": 
-        context.baseURL = "https://www.dev.com" 
-    elif inputENV == "prod":
-        context.baseURL = "https://www.prod.com"
-    print(context.baseURL)
+    
 
 @given("user starts the browser")
 def step_impl(context):
@@ -27,11 +19,29 @@ def step_impl(context):
 @then("user should navigate to Home Page")
 def step_impl(context):
     print("Then user should navigate to Home Page")
-    url = context.baseURL
-    print(url)
+    inputENV = context.config.userdata.get("ENV")
+    print("Environment: {inputENV}")
+    if inputENV == "sys":
+        context.baseURL = "https://www.sys.com"
+    elif inputENV == "dev": 
+        context.baseURL = "https://www.dev.com" 
+    elif inputENV == "prod":
+        context.baseURL = "https://www.prod.com"
+    print(context.baseURL)
+    # url = context.baseURL
+    # print(url)
 
 @then("user should not navigate to Home Page")
 def step_impl(context):
     print("Then user should not navigate to Home Page")
-    url = context.baseURL
-    print(url)
+    # url = context.baseURL
+    # print(url)
+    inputENV = context.config.userdata.get("ENV")
+    print("Environment: {inputENV}")
+    if inputENV == "sys":
+        context.baseURL = "https://www.sys.com"
+    elif inputENV == "dev": 
+        context.baseURL = "https://www.dev.com" 
+    elif inputENV == "prod":
+        context.baseURL = "https://www.prod.com"
+    print(context.baseURL)
